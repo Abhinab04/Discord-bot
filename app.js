b4166d4b4c07aa10bee7c46d18f40bfa7615f3ed
ABHINAB SHARMA <146541054+Abhinab04@users.noreply.github.com>
2024-08-24 18:30:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -79,9 +79,9 @@ client.on('ready',async()=>{
         .setTitle(title)
         .setURL(ans)
         .setDescription(text)
-        client.channels.cache.get("1237466068281458742").send("@everyone"+"  "+ item);
-       client.channels.cache.get("1237466068281458742").send({ embeds: [coloring] });
+        client.channels.cache.get("channel_id").send("@everyone"+"  "+ item);
+       client.channels.cache.get("channel_id").send({ embeds: [coloring] });
     }
   }
 })
-client.login(process.env.DISCORD_TOKEN);
+client.login(process.env.DISCORD_TOKEN);  // (DISCORD_TOKEN) will be the token created by the discord and store that in .env file
